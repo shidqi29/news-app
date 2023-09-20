97f783a5984ba2d6af6e9a4372138cb196e67c0d
shidqi29 <shidqiamirulhaq@gmail.com>
2023-09-20 23:42:43 +0700

    feat: initial commit �

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Hello World</h1>
+      <p className="">TEST</p>
+      <button className="btn btn-outline">My Button</button>
+    </>
+  );
+}
+
+export default App;
