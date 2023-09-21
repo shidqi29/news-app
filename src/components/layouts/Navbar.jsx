6c8e536de1a9e9870f3db549e17abca98fc19024
shidqi29 <shidqiamirulhaq@gmail.com>
2023-09-21 23:51:45 +0700

    feat(navbar): add Navbar functionality and styling

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,8 +1,62 @@
+import { Link, NavLink } from "react-router-dom";
+import {
+  PiBookmarksSimpleBold,
+  PiBookmarksSimpleFill,
+  PiMagnifyingGlassBold,
+  PiMoonBold,
+  PiSunDimBold,
+} from "react-icons/pi";
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
+
 const Navbar = () => {
+  const isActive = ({ isActive }) => {
+    return isActive ? (
+      <div className="rounded-full border border-base-content p-2">
+        <PiBookmarksSimpleFill size={28} />
+      </div>
+    ) : (
+      <div className="p-2">
+        <PiBookmarksSimpleBold size={28} />
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    themeChange(false);
+  }, []);
+
   return (
-    <header>
-      <nav>
-        <h1>Navbar</h1>
+    <header className="mb-2 w-full bg-base-300 py-2 shadow-sm shadow-current">
+      <nav className="container navbar mx-auto">
+        <div className="flex-1">
+          <div className="text-3xl">
+            <Link to="/">News App</Link>
+          </div>
+        </div>
+        <div className="flex-none gap-5">
+          <form>
+            <div className="join">
+              <input
+                className="input join-item input-bordered rounded-full"
+                placeholder="Search"
+              />
+              <button className="btn btn-neutral join-item rounded-r-full">
+                <PiMagnifyingGlassBold size={28} />
+              </button>
+            </div>
+          </form>
+          <label className="swap swap-rotate">
+            <input type="checkbox" />
+            <div className="swap-on w-7 fill-current" data-set-theme="light">
+              <PiMoonBold size={28} />
+            </div>
+            <div className="swap-off w-7 fill-current" data-set-theme="dark">
+              <PiSunDimBold size={28} />
+            </div>
+          </label>
+          <NavLink to="/saved">{isActive}</NavLink>
+        </div>
       </nav>
     </header>
   );
