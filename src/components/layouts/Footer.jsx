ddbf107c810766ea02f6b215a4573284ce9e8770
shidqi29 <shidqiamirulhaq@gmail.com>
2023-09-29 10:15:25 +0700

    feat: add footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,7 +1,20 @@
 const Footer = () => {
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className="footer footer-center mt-20 border-t border-current bg-base-300 py-8 text-base-content">
+      <aside>
+        <p className="text-base">
+          Built with ❤ by <span className="font-semibold">Shidqi</span>. The
+          source code is available on{" "}
+          <a
+            href="https://github.com/shidqi29/news-app"
+            target="_blank"
+            className="underline"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </p>
+      </aside>
     </footer>
   );
 };
